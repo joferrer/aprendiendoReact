21b30e2a2a216c585aa6c86e79619d004b9e841b
Jeison Ferrer <jeison120120@hotmail.com>
2022-07-11 18:32:57 -0500

    Introducción a jtest y las pruebas unitarias e integración

---
+++ hola-mundo/test/demo.test.js
@@ -0,0 +1,15 @@
+describe('Pruebas en el <HeaderComponent />',()=>{
+    test('Primer test', () => { 
+
+        const m = 'Hola mundo';
+    
+        
+        const m2 = '  Hola mundo  '.trim();
+    
+    
+        expect(m).toBe(m2);
+    
+     });
+
+});
+

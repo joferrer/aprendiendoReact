1af258c16df39b52c94dd0a2d9f8af2994744057
Jeison Ferrer <jeison120120@hotmail.com>
2022-08-06 21:13:35 -0500

    Nav bar y control de links

--- HooksApp/src/09-context/MainApp.jsx
+++ HooksApp/src/09-context/MainApp.jsx
@@ -2,13 +2,15 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import { AboutPage } from "./AboutPage"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
+import { NavBar } from "./NavBar"
 
 
 export const MainApp = () => {
   return (
     <>
-        <h1>Main App</h1>
-        <hr />
+        <NavBar/>
+
+        
 
         <Routes>
             <Route path="/" element = {<HomePage />} />
